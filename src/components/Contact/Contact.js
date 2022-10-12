cc5ed739d3c16aadf3cefeae9d2733292dbf9510
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-11 23:56:43 -0300

    quitado: __blank

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
     <div className="contact-section">
         Nos podes encontrar en: San Bernardo, La Costa. Tucuman 2943.
         <div className="social-media-container">
-            <a href="https://www.instagram.com/mibebep/" target="_blank" rel="noopener noreferrer"><FaInstagram className="social-media-icons ig-icon" /></a>
-            <a href="https://www.facebook.com/fabrizzio.panalera" target="_blank" rel="noopener noreferrer"><FaFacebook className="social-media-icons fb-icon"/></a>
+            <a href="https://www.instagram.com/mibebep/"><FaInstagram className="social-media-icons ig-icon" /></a>
+            <a href="https://www.facebook.com/fabrizzio.panalera"><FaFacebook className="social-media-icons fb-icon"/></a>
         </div>
     </div>
     )
