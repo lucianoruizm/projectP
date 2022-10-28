ae1719aea20c307fd89c819156cb911234acbebb
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-27 21:08:32 -0300

    addFavs

--- src/components/Heart/Heart.js
+++ src/components/Heart/Heart.js
@@ -1,53 +1,97 @@
 import { useState } from "react";
-import { BsFillSuitHeartFill } from 'react-icons/bs';
+import { BsSuitHeartFill, BsSuitHeart } from 'react-icons/bs';
 import './Heart.css';
 
 export const Heart = ({ productCard })=>{
 
     const [favs, setFavs] = useState([]);
-    let className = 'heart__icon';
+    // let className = 'heart__icon';
 
-    // function handleClick (e) {
-    //   e.preventDefault();
-    //   setLike(!like);
-    //   if(like === true) {
-    //     addProductLocalStorage(card);
+    // // function handleClick (e) {
+    // //   e.preventDefault();
+    // //   setLike(!like);
+    // //   if(like === true) {
+    // //     addProductLocalStorage(card);
+    // //   }
+    // // }
+
+    // // if (like === true) {
+    // //   className += ' active';
+    // // }
+
+    // const onAdd = (product) => {
+    //   const exist = favs.find(x => x.id === product.id);
+    //   if(exist){
+    //     setFavs([...favs])
+    //     console.log('ya esta agregado');
+    //   } else {
+    //   setFavs([...favs, {...product}]);
+    //   console.log('agregado a favs');
+    //   localStorage.setItem("favs", JSON.stringify(productCard.id));
+
     //   }
+    //   console.log(favs);
     // }
 
-    // if (like === true) {
-    //   className += ' active';
+    // function getProductLocalStorage() {
+    //   let productList;
+    //   if(localStorage.getItem('productList') === null){
+    //     productList = [];
+    //   } else{
+    //     productList = JSON.parse(localStorage.getItem('productList'))
+    //     console.log(productList);
+    //   }
+    //   return productList;
     // }
 
-    const onAdd = (product) => {
-      const exist = favs.find(x => x.id === product.id);
-      if(exist){
-        setFavs([...favs])
-        console.log('ya esta agregado');
+    // function addProductLocalStorage(product) {
+    //   let productList = getProductLocalStorage()
+    //   productList.push(product);
+    //   localStorage.setItem('productList', JSON.stringify(productList))
+    //   console.log(productList);
+    // }
+
+    const onAdd = () => {
+      let array = favs;
+      let addArray = true;
+      array.map((item, key) => {
+        if (item === productCard.id) {
+          array.splice(key, 1);
+          addArray = false;
+        }
+        return addArray;
+      });
+      if (addArray) {
+        array.push(productCard.id);
+      }
+      setFavs([...array])
+      console.log(addArray);
+      console.log(favs);
+
+      localStorage.setItem("favorites", JSON.stringify(favs));
+
+      let storage = localStorage.getItem('favItem' + (productCard.id) || '0');
+      if (storage == null) {
+        localStorage.setItem(('favItem' + (productCard.id)), JSON.stringify(productCard));
       } else {
-      setFavs([...favs, {...product}]);
-      console.log('agregado a favs');
-      addProductLocalStorage(productCard);
+        localStorage.removeItem('favItem' + (productCard.id));
       }
     }
 
-    function getProductLocalStorage() {
-      let productList;
-      if(localStorage.getItem('productList') === null){
-        productList = [];
-      } else{
-        productList = JSON.parse(localStorage.getItem('productList'))
-      }
-      return productList;
-    }
-
-    function addProductLocalStorage(product) {
-      let productList = getProductLocalStorage()
-      productList.push(product);
-      localStorage.setItem('productList', JSON.stringify(productList))
-    }
+    
   
     return (
-      <BsFillSuitHeartFill className={className} onClick={onAdd} />
+      <>
+        {favs.includes(productCard.i) ? (<BsSuitHeartFill 
+          onClick={() => onAdd({ productCard })}
+          style={{ color: 'red'}}
+        />
+        ) : (
+              <BsSuitHeart
+              onClick={() => onAdd({ productCard })}
+                style={{ color: 'red'}} 
+              />
+            )}
+      </>
     )
 }
