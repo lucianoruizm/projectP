6604d054ec97058704bc801e700e4477247d1603
Luciano Ruiz <lucianoruizm993@gmail.com>
2023-04-03 23:06:45 -0300

    estilos

--- src/components/SlideshowGallery/SlideshowGallery.js
+++ src/components/SlideshowGallery/SlideshowGallery.js
@@ -1,77 +1,27 @@
-import bgimg from '../../images/bg-pmbb.jpg';
-import bgimg2 from '../../images/bg-pmbb2.jpg';
-import bgimg3 from '../../images/bg-pmbb3.jpg';
-import bgimg4 from '../../images/bg-pmbb4.jpg';
-
-import React, { useState } from 'react';
 import './SlideshowGallery.css';
-import Carousel from 'react-bootstrap/Carousel';
 
 export const SlideshowGallery = () => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={bgimg}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <div className="text-presentation-container img1">
-            <h3>Bienvenidos a Pañalera Mi Bebé!</h3>
-            <p>Pañales, ropa y accesorios para los más pequeñitos.</p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={bgimg2}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <div className="text-presentation-container">
-            <h3>¿Ya sabés que pañal vas a elegir para tu recién nacido?</h3>
+    <div className='headMainContainer'>
+        <div className='mainTitle'>
+           <h3>Bienvenidos a Pañalera Mi Bebé!</h3>
+           <p>Pañales, ropa y accesorios para los más pequeñitos.</p>
+        </div>
+        <div className="gridTitles">
+          <div className='items'>
+            <h4>¿Ya sabés que pañal vas a elegir para tu recién nacido?</h4>
             <p>En pañalera Mi Bebé, podés contar con diversas marcas.</p>
           </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={bgimg3}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <div className="text-presentation-container">
-            <h3>Ropa para tu pequeño/a!</h3>
+          <div className='items'>
+            <h4>Ropa para tu pequeño/a!</h4>
+            <p>Encontra el conjunto que mejor se adapte.</p>
           </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={bgimg4}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <div className="text-presentation-container">
-            <h3>También contamos con los mejores tejidos!</h3>
-            <p>
-              Desde gorros hasta peluches!
-            </p>
+          <div className='items'>
+            <h4>También contamos con los mejores tejidos!</h4>
+            <p>Desde gorros hasta peluches.</p>
           </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        </div>
+    </div>
   );
 }
